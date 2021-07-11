649da7753f2cf92dbb3ff38145772df8e2ec819f
Vinay <86608961+vinaydbharadwaj@users.noreply.github.com>
2021-07-11 18:19:35 +0530

    Add files via upload

---
+++ DataEntry with MongoDB/index.js
@@ -0,0 +1,56 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var age = req.body.age;
+    var club = req.body.club;
+    var country = req.body.country;
+
+    var data = {
+        "name": name,
+        "age" : age,
+        "club": club,
+        "country" : country
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('dataentry_success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
